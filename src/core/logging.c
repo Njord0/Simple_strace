60522f713d7ddfc6d5acc96450e52facc9817d22
Njord0 <nicolas.cochet65000@gmail.com>
2020-09-01 18:11:02 +0200

    Added logging

---
+++ src/core/logging.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "logging.h"
+
+void log(unsigned int type, const char* message)
+{
+    if (type == L_ERROR)
+    {
+        fprintf(stderr, "%s\n", message);
+        exit(L_ERROR);
+    }
+
+    if (type == L_WARNING)
+        fprintf(stderr, "%s\n", message);
+
+    else if (type == L_LOG)
+        fprintf(stdout, "%s\n", message);
+
+    return;
+}
